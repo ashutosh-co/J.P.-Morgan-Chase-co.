6b2a7840facd49e08b09f8b3c982996cf1aad428
Ashutosh Shelke <111786950+ashutoshSnj@users.noreply.github.com>
2024-10-31 19:17:05 +0530

    Update q2.c

--- q2.c
+++ q2.c
@@ -1,28 +1,5 @@
 #include<stdio.h>
-/*void main(){
-	printf("Enter the size = ");
-	int size;
-	scanf("%d",&size);
-	int arr[size];
-	for(int i=0;i<size;i++){
-		scanf("%d",&arr[i]);
-	}
-	int j=0;
-	for(int i=0;i<size;i++){
-		if(arr[i]!=0){
-		    arr[j]=arr[i];
-		    j++;
-		}
-	}
-	for(int i=j;i<size;i++){
-		arr[i]=0;
-	}
-	for(int i=0;i<size;i++){
-		printf(" %d",arr[i]);
-	}
-	
-}*/
-// Another way
+
 
 #include <stdio.h>
 void main()
